3a41af5a9005000e3d90c23d871214a03b23d341
MetalJames <volodyaruzhak@gmail.com>
2024-01-29 10:20:17 -0500

    # Done set up and done first step(can provide pet type and name)

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Step 1: Pet Creation
+Console.WriteLine("Welcome to the Virtual Pet Simulator!");
+
+int choiceOne = 1;
+int choiceTwo = 2;
+int choiceThree = 3;
+
+// Allow the user to choose a pet type
+Console.WriteLine("Please choose a type of pet:");
+Console.WriteLine($"{choiceOne}. Cat");
+Console.WriteLine($"{choiceTwo}. Dog");
+Console.WriteLine($"{choiceThree}. Rabbit");
+
+Console.WriteLine("User Input: ");
+int petTypeChoice = Convert.ToInt32(Console.ReadLine());
+
+switch (petTypeChoice)
+{
+    case 1:
+        Console.WriteLine("You've chosen a Cat. What would you like to name your pet?");
+        break;
+    case 2:
+        Console.WriteLine("You've chosen a Dog. What would you like to name your pet?");
+        break;
+    case 3:
+        Console.WriteLine("You've chosen a Rabbit. What would you like to name your pet?");
+        break;
+    default:
+        Console.WriteLine("Wrong input");
+        break;
+}
+
+string petType = Console.ReadLine();
+
+// Give the pet a name
+Console.Write("Give your pet a name: ");
+string petName = Console.ReadLine();
+
+// Display a welcome message
+Console.WriteLine($"\nCongratulations! You now have a {petType} named {petName}.");
